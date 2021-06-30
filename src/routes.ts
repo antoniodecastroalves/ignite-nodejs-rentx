2592bbf7fd8f9967d4f335150040abd867617256
Antonio Alves <antoniodecastroalves@gmail.com>
2021-06-30 11:18:19 -0300

    adding types

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response) {
+
+  CreateCourseService.execute({
+    name: "NodeJS", 
+    educator: "Dani",
+    duration: 10
+  });
+
+  return response.send();
+
+}
