5fccfaac9d6f67e6548a244258b72b903fb6fc4e
Antonio Alves <antoniodecastroalves@gmail.com>
2021-08-23 23:53:39 -0300

    configuring ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express, { request, response } from 'express';
-import { createCourse } from './routes';
+import express from "express";
 
 const app = express();
 
-app.get("/", createCourse);
+app.use(express.json());
 
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World!" });
+});
 
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running!"));
