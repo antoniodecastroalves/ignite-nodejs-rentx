518e293347d99b77a051e5fda3e3fb467b50af7f
Antonio Alves <antoniodecastroalves@gmail.com>
2021-08-26 14:46:21 -0300

    creating category repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category: Category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      create_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
