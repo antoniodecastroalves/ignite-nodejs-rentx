dd43295d1d7d46de4324ded93f607bc1ad2e9b76
Antonio Alves <antoniodecastroalves@gmail.com>
2021-08-28 22:48:07 -0300

    knowing Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,8 +7,17 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
+  private static INSTANCE: CategoriesRepository;
+
   constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
